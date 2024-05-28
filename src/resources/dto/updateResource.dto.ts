0ffa5dee7273e3cea3ce2d0eb1ea4372a9644e6f
Nirob Hasan <thenirobhasan@gmail.com>
2024-05-28 17:54:04 +0600

    Path changed

--- src/resources/dto/updateResource.dto.ts
+++ src/resources/dto/updateResource.dto.ts
@@ -1,5 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateResourceDto } from "./createResource.dto";
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateResourceDto } from './CreateResource.dto';
 
-
-export class UpdateResourceDto extends PartialType(CreateResourceDto){}
+export class UpdateResourceDto extends PartialType(CreateResourceDto) {}
