ec718599ac3c6886097b7b999a1a270e05f35ec2
Nirob Hasan <thenirobhasan@gmail.com>
2024-06-13 15:10:04 +0600

    Cors addded

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: true,
+        credentials: true
+    });
     app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe());
     // app.useGlobalInterceptors(new SuccessInterceptor());
