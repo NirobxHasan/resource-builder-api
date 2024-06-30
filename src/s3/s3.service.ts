b2f60cc113db232dfe6f84b7c60d6beff72ec152
Nirob Hasan <thenirobhasan@gmail.com>
2024-06-30 13:47:38 +0600

    Remove AWS access keys from source code

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -9,6 +9,8 @@ export class S3Service {
 
     constructor() {
         this.s3 = new AWS.S3({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
             region: process.env.AWS_REGION
         });
         this.bucketName = process.env.AWS_S3_BUCKET_NAME;
